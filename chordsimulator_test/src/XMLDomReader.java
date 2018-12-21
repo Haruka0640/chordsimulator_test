d7eac3dfe01d1e8a512ae299af3a5d613fa1e2b9
Haruka <izumi.ha@nttdata-gsl.co.jp>
2018-12-21 20:54:03 +0900

    change method to read XML

---
+++ chordsimulator_test/src/XMLDomReader.java
@@ -0,0 +1,26 @@
+package def;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+public class XMLDomReader {
+
+	public static Document buildDocument(String file){
+	    try {
+	        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+	        Document document = docBuilder.parse(file);
+	        return document;
+	    } catch (ParserConfigurationException | SAXException | IOException ex) {
+	        System.out.println("the exception is: " + ex.toString());
+	    }
+	    return null;
+	}
+}
--- chordsimulator_test/src/main.java
+++ chordsimulator_test/src/main.java
@@ -2,14 +2,10 @@ package def;
 import java.io.*;
 import javax.sound.midi.*;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.*;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 
 import java.util.ArrayList;
 
@@ -45,14 +41,12 @@ class MidiTest {
     	intRoot = changeNoteToNum(strRoot,octave);
     	chordArray.add(intRoot); 
     	
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse("src/src/resource/chordnumbers.xml");
-        
-        Element chordElement = document.getElementById("M");
-        NodeList semitonesNodeList = chordElement.getElementsByTagName("semitone");
-        
-    	for (int j = 0; j < semitonesNodeList.getLength(); j++) {
+        Document document = XMLDomReader.buildDocument("src/src/resource/chordnumbers.xml");
+    	       
+        Element chordElement = document.getElementById(strChordName);
+        NodeList semitonesNodeList = chordElement.getElementsByTagName("semitone");        
+
+        for (int j = 0; j < semitonesNodeList.getLength(); j++) {
     		Node semitoneNode = semitonesNodeList.item(j);
     		if(semitoneNode.getNodeType() == Node.ELEMENT_NODE) {
     			Element semitoneElement = (Element)semitoneNode;
@@ -62,32 +56,7 @@ class MidiTest {
 	    	}
     	}
     	
-    	return chordArray;
-
-        
-//    	Element root = document.getDocumentElement();
-//    	NodeList chordsNodeList = root.getChildNodes();
-        
-//    	for (int i = 0; i < chordsNodeList.getLength(); i++) {
-//    		Node chordNode = chordsNodeList.item(i);
-//    		if(chordNode.getNodeType() == Node.ELEMENT_NODE) {
-//    			Element chordElement = (Element)chordNode;
-//	   			if(chordElement.getElementsByTagName("shortname").item(0).getTextContent().equals(strChordName)) {
-//	   				NodeList semitonesNodeList = chordElement.getElementsByTagName("semitone");
-//	   		    	for (int j = 0; j < semitonesNodeList.getLength(); j++) {
-//	   		    		Node semitoneNode = semitonesNodeList.item(j);
-//	   		    		if(semitoneNode.getNodeType() == Node.ELEMENT_NODE) {
-//	   		    			Element semitoneElement = (Element)semitoneNode;
-//	   		    			intSemitone = Integer.parseInt(semitoneElement.getTextContent());
-//	   		    			intSemitone = intRoot + intSemitone;
-//	   		    			chordArray.add(intSemitone);
-//	   		    		}
-//	   		    	}
-//	   				break;
-//	    		}
-//    		}
-//    	}
-    	
+    	return chordArray;   	
 
     }
     
@@ -95,31 +64,11 @@ class MidiTest {
  
     	int intNote = 0;
     	
-    	DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse("src/src/resource/notenumbers.xml");
-        
-//        Element element = document.getElementById("M");
-//        
-//        
-//        intNote = Integer.parseInt(element.getElementsByTagName("number").item(0).getTextContent());
-//        
-//        System.out.print(intNote);
-        
-    	Element root = document.getDocumentElement();
-    	NodeList notesNodeList = root.getChildNodes();
+    	Document document = XMLDomReader.buildDocument("src/src/resource/notenumbers.xml");
 
-    	for (int i = 0; i < notesNodeList.getLength(); i++) {
-    		Node noteNode = notesNodeList.item(i);
-    		if(noteNode.getNodeType() == Node.ELEMENT_NODE) {
-    			Element noteElement = (Element)noteNode;
-	   			if(noteElement.getElementsByTagName("name").item(0).getTextContent().equals(strNote)) {
-	    			intNote = Integer.parseInt(noteElement.getElementsByTagName("number").item(0).getTextContent());
-	    			break;
-	    		}
-    		}
-    	}
-    	
+        Element noteElement = document.getElementById(strNote);
+        intNote = Integer.parseInt(noteElement.getElementsByTagName("number").item(0).getTextContent());
+
     	intNote = intNote + octave * 15;
         return intNote;
         
